a2691692bf2bd4061235ea0245ee72615ad7297b
ServercoinOfficial <123994746+thetruecutepuppydog@users.noreply.github.com>
2024-08-31 13:39:55 -0400

    Update Servercoindeployapp.py

--- Servercoindeployapp.py
+++ Servercoindeployapp.py
@@ -10888,24 +10888,41 @@ with open("changethe600thing.txt","w") as file:
 with open("countdownthing.txt","w") as file:
     file.write(str(countdownthing))
 def loop1():
+ def t600thingactivate():
+     the600thing = serverthingthing.the600get()
+     if the600thing<-1:
+        serverthingthing.the600reset()
+     serverthingthing.the600fix()
+     return str(the600thing)
+ def countdownthingactivate():
+     countdownthing = serverthingthing.thecountdownget()
+     serverthingthing.thecountdownfix()
+
+
+     if countdownthing <0.25:
+
+               serverthingthing.acceptablockpuppy()
+               serverthingthing.thecountdownreset()
+
+
+               serverthingthing.the600reset()
+
+     return str(countdownthing)
+ print("DOES THIS EVEN DO ANYTHING")
  the600thing = 0
  timeatstarttime = 0
- with open("timeatstart.txt","r") as file:
-    try:
-     timeatstarttime = float(file.read())
-    except:
-        lol = True
- totaltime = time.time()-timeatstarttime
+ with open("changethe600thing.txt","w") as file:
+     file.write(str(600))
+ totaltime = 3
  with open("changethe600thing.txt","r") as file:
     fileread = str(file.read())
-
     if float(fileread)>0:
      themega600thing = float(fileread)-totaltime
-     
+
      if themega600thing<0 and themega600thing>=-3:
          countdownthing = ConvertTheNumber(themega600thing)-3
          if countdownthing<0:
-             the600thing = 6+countdownthing
+             the600thing = 600+countdownthing
              with open("changethe600thing.txt","w") as file:
                  file.write(str(the600thing))
          with open("countdownthing.txt","w") as file:
@@ -10922,72 +10939,74 @@ def loop1():
                  file.write(str(countdownthing))
          else:
           the600thing = ConvertTheNumber(themega600thing)
-         
-         
+
+
           with open("changethe600thing.txt","w") as file:
              file.write(str(the600thing))
-         
+
     else:
         with open("countdownthing.txt","r") as file:
             themegacountdownthing = float(file.read())-totaltime
             themegacountdownthing = ConvertTheNumber(themegacountdownthing)-2
             #if it is 2 off switch 2 to four
-            
+
             the600thing = themegacountdownthing
             with open("changethe600thing.txt","w") as file:
                 file.write(str(the600thing))
+ averagetimelist = 0
+ averagetimecount = 0
  while True:
      while the600thing>0:
+         starttime = time.time()
          time.sleep(0.25)
-         ipthing = get_local_ip2()
-         DATA={}
+
          try:
-          response23 = requests.post("http://"+str(ipthing)+":"+str(SPECIALPORT)+"/checkthe600thing",json=DATA)
+          response23 = ""
+          if SpecialDevice == 2:
+           response23 = t600thingactivate()
+       
+           the600thing = float(response23)
+           countdownthing = 3
+          else:
+           response23 =t600thingactivate()
+        
+           try:
+            response23 = response23.json()
+           except Exception as e:
+               print("THAT'S NONSENSE: "+str(e))
           
-          response23 = response23.json()
-          response23 = response23["Success"]
-          the600thing = float(response23)
-          
-          countdownthing = 3
+           the600thing = float(response23)
+           countdownthing = 3
 
          except Exception as e:
              lol=True
-     
+         endtime = time.time()
+         truetime = endtime-starttime
+         averagetimelist+=truetime
+         averagetimecount+=1
+         trueaveragetime = averagetimelist/averagetimecount
      ipthing = get_local_ip2()
-     try:
-      requests.post("http://"+str(ipthing)+":"+str(SPECIALPORT)+"/gothroughtransactions")
-     except:
-         lol=True
+
      loadloop = True
      numthing=3
-     while numthing>0.3:
+     while numthing>0:
       time.sleep(0.25)
       try:
-       response222 = requests.get("http://"+str(ipthing)+":"+str(SPECIALPORT)+"/getthecountdownthing")
-       if response222.status_code == 200:
-        responsedata = response222.json()
-        numthing = float(responsedata["Success"])
-        if numthing<0.3:
+       response222 = ""
+       response222 = countdownthingactivate()
+
+       responsedata = response222
+       numthing = float(responsedata)
+       if numthing<0.3:
           loadloop = False
-          try:
-           requests.post("http://"+str(ipthing)+":"+str(SPECIALPORT)+"/checkthecountdowthing")
-          except:
-              lol=True
-          with open("changethe600thing.txt","r") as file:
-              the600thing = float(file.read())
-          the600thing = 6
-          with open("changethe600thing.txt","w") as file:
-                  file.write(str(the600thing))
-          break
-        else:
-         try:
-          response666 = requests.post("http://"+str(ipthing)+":"+str(SPECIALPORT)+"/checkthecountdowthing")
-         except:
-             lol=True
-      except:
+
+          serverthingthing.the600reset()
+          the600thing = serverthingthing.the600get()
+       else:
          lol=True
 
-
+      except Exception as e:
+         lol=True
 
 
 def loop3(timethingthing,timewaitthing):
