bcc0f104e2bf0e9854dd353b5b7c91dc077ccd04
ServercoinOfficial <123994746+Servercoin@users.noreply.github.com>
2024-11-16 13:51:24 -0500

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,15 +1,15 @@
+
 from flask import request, jsonify, Flask
 from flask_cors import CORS
 import requests
 app = Flask(__name__)
 url = input("URL you will use as this is a proxy to let you use gunicorn.")
 CORS(app)
-@app.route("/cutepuppies",methods=['POST'])
-def cutepuppies():
-    data = request.json
-    url = data["URL"]
-
-    newdata = requests.get(url)
+@app.route("/<path:path>",methods=['GET'])
+def cutepuppies(path):
+    
+    print("PAth: "+str(path))
+    newdata = requests.get(url+"/"+path)
     newdata = newdata.json()
     newdata = newdata["Success"]
     return jsonify({"Success":newdata})
